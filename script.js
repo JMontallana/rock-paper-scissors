8f8fdc94dfa19aa2fa845192c44c1deff18ed874
JMontallana <jeffrymontallana28@gmail.com>
2023-02-13 04:03:00 +0800

    practicing dom

--- script.js
+++ script.js
@@ -1,58 +1,28 @@
 const choices = ['rock', 'paper', 'scissor'];
+const computerChoiceDisplay = document.querySelector('.computer');
+const playerChoiceDisplay = document.querySelector('.player');
+const buttons = document.querySelectorAll('button');
+const finalResult = document.querySelector('.final-result');
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    winner = getWinner(playerSelection, computerSelection);
-    console.log(winner)
+let useChoice;
+
+function handleClick(e) {
+    useChoice = e.target.id;
+    playerChoiceDisplay.textContent = useChoice;
+    getComputerChoice();
 }
+
+buttons.forEach((button) => {
+    button.addEventListener('click', handleClick)
+});
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)]
+    const computer = choices[Math.floor(Math.random() * choices.length)];
+    computerChoiceDisplay.textContent = computer;
 }
 
-function validateChoice(choice) {
-    return choices.includes(choice)
-}
 
-function getPlayerChoice() {
-    
-    let input = prompt("Enter rock, paper, scissor");
-    while ( input == null ) {
-        input = prompt("Enter rock, paper or scissor");
-    }
-    input = input.toLowerCase();
-    let check = validateChoice(input);
-    console.log(check)
-    while (check == false) {
-        input = prompt("Invalid input. Please enter rock, paper or scissor");
-        while ( input == null ) {
-            input = prompt("Enter rock, paper or scissor");
-        }
-        input = input.toLowerCase();
-        check = validateChoice(input);
-    }
-    return input;
-}
 
-function getWinner(choicePlayer, choiceComputer) {
-    if (choicePlayer === choiceComputer) {
-        return `Its a tie ${choiceComputer} Ties with ${choicePlayer}`
-    } else
-        if ((choicePlayer == "rock" && choiceComputer == "scissor") ||
-            (choicePlayer == "paper" && choiceComputer == "rock") ||
-            (choicePlayer == "scissor" && choiceComputer == "paper")) {
-                return `Player wins ${choiceComputer} is beaten by ${choicePlayer}`
-            } else {
-                return `Computer wins ${choicePlayer} is beaten by ${choiceComputer}`
-            }
-}
 
-function game() {
-    for ( let i = 1; i <= 5; i++) {
-        playRound();
-    }
-}
-game();
 
 
