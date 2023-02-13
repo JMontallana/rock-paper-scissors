c94ed7c89b9da0907faeb721bd497fb85b1e0f03
JMontallana <jeffrymontallana28@gmail.com>
2023-02-13 19:34:24 +0800

    added comments and function to reset the game and declace a winner

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const buttons = document.querySelectorAll('button');
 const playerScore = document.querySelector("#player-score");
 const computerScore = document.querySelector("#computer-score");
 const finalResult = document.querySelector('.final-result');
+const finalResultStyle = document.querySelector('.final-result').style;//use to style dom
 
 let userChoice;
 let computerChoice;
@@ -14,26 +15,48 @@ let compScore = 0;
 computerScore.textContent = compScore;
 playerScore.textContent = userScore;
 
+//event to handle the function everytime we press button
 function handleClick(e) {
     userChoice = e.target.id;
     playerChoiceDisplay.textContent = userChoice;
     getComputerChoice();
     getResult(userChoice, computerChoice);
-    
+
+    //solution to reset the game when it reach to five wins
+    if(userScore == 5) {
+        compScore = 0;
+        userScore = 0;
+        computerScore.textContent = compScore; //set score 
+        playerScore.textContent = userScore; //set score
+        finalResult.textContent = 'Congratulations Player wins';
+        finalResultStyle.color = 'Green';
+        
+    } else
+        if (compScore == 5){
+            compScore = 0;
+            userScore = 0;
+            computerScore.textContent = compScore;
+            playerScore.textContent = userScore;
+            finalResult.textContent = 'Game Over! Computer wins';
+            finalResultStyle.color = 'Red';    
+        }
 }
 
 buttons.forEach((button) => {
     button.addEventListener('click', handleClick)
 });
 
+//get a random value from array choices
 function getComputerChoice() {
     const computer = choices[Math.floor(Math.random() * choices.length)];
     computerChoice = computer;
     computerChoiceDisplay.textContent = computerChoice;
 }
 
+//sulotion to get the result 
 function getResult(player, computer) {
     if (player === computer) {
+        finalResultStyle.color = 'black';
         finalResult.textContent = 'Tie';
         playerScore.textContent = userScore;
         computerScore.textContent = compScore;
@@ -41,13 +64,17 @@ function getResult(player, computer) {
         if ((player == 'rock' && computer == 'scissor') ||
             (player == 'paper' && computer == 'rock') ||
             (player == 'scissor' && computer == 'paper')) {
+                finalResultStyle.color = 'black';
                 finalResult.textContent = 'You win';
-                playerScore.textContent = userScore++ +1;
+                playerScore.textContent = userScore++ +1; //added +1 because default value is 0
             } else {
+                finalResultStyle.color = 'black';
                 finalResult.textContent = 'Computer win';
-                computerScore.textContent = compScore++ +1;
+                computerScore.textContent = compScore++ +1; 
             }
 }
+
+
 
 
 
